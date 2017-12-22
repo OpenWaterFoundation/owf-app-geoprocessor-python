86877c99a7292488239857e99d8c90a8967ab193
Emma Giles <ej.giles20@gmail.com>
2017-12-22 14:42:07 -0700

    Added pycharm for QGIS bat file

---
+++ src/general/start-pycharm-with-qgis-env/PyCharm for QGIS.bat
@@ -0,0 +1,37 @@
+rem Bath file to set the environment for running pycharm consistent with QGIS environment
+rem - Use QGIS Python and libraries
+rem - Start pycharm with the configured environment
+
+rem The following seems to be more relevant.
+rem See:  http://spatialgalaxy.com/2014/10/09/a-quick-guide-to-getting-started-with-pyqgis-on-windows/
+rem The following provides useful background but seems incomplete.
+rem See:  http://planet.qgis.org/planet/tag/pycharm/
+
+@echo off
+rem Where QGIS is installed
+SET OSGEO4W_ROOT=C:\OSGeo4W64
+rem Set the QGIS environment by calling the setup script that is distributed with QGIS 
+CALL %OSGEO4W_ROOT%\bin\o4w_env.bat
+
+@echo off
+rem Name of QGIS program to run 
+SET QGISNAME=qgis
+rem Absolute path to QGIS program to run 
+SET QGIS=%OSGEO4W_ROOT%\apps\%QGISNAME%
+rem Not sure what the following is used for but include in case PyCharm or QGIS uses 
+SET QGIS_PREFIX_PATH=%QGIS%
+rem Set the absolute path to PyCharm program 
+rem SET PYCHARM="C:\Program Files (x86)\JetBrains\PyCharm 3.0\bin\pycharm.exe"
+SET PYCHARM="C:\Program Files (x86)\JetBrains\PyCharm Community Edition 2016.2.3\bin\pycharm.exe"
+
+rem Add QGIS to the PATH environmental varibale so taht all QGIS, GDAL, OGR, etc. programs are found
+SET PATH=%PATH%;%QGIS%\bin
+
+rem Add pyQGIS libraries to the PYTHONPATH so that they are found by Python
+set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\qgis\python;
+set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\qgis\python\plugins;
+set PYTHONPATH=%PYTHONPATH%;%OSGEO4W_ROOT%\apps\Python27\Lib\site-packages;
+
+rem Start the PyCharm IDE, /B indicates to use the same windows
+rem Command line parameters passed to this script will be passed to PyCharm 
+start "PyCharm aware of QGIS" /B %PYCHARM% %*

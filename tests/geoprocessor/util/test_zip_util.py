16d616c29fa7dcde6628f4ed7e5b3038ae13632e
jurentie <jurentie@gmail.com>
2019-02-26 16:59:31 -0700

    working on test_zip_util

---
+++ tests/geoprocessor/util/test_zip_util.py
@@ -0,0 +1,9 @@
+import pytest
+import geoprocessor.util.zip_util as zip_util
+import os
+
+
+# def test_is_tar_file_true(tmpdir):
+#     file = tmpdir.mkdir("sub").join("test_file.tar")
+#     print(file)
+#     #assert zip_util.is_tar_file(file) == True

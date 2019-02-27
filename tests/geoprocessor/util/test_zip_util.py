4e0beedfdff84149e48d60f552a520eefbc0283c
jurentie <jurentie@gmail.com>
2019-02-27 10:19:45 -0700

    working on adding tests

--- tests/geoprocessor/util/test_zip_util.py
+++ tests/geoprocessor/util/test_zip_util.py
@@ -1,9 +1,13 @@
 import pytest
 import geoprocessor.util.zip_util as zip_util
 import os
+import tarfile
 
-
-# def test_is_tar_file_true(tmpdir):
-#     file = tmpdir.mkdir("sub").join("test_file.tar")
-#     print(file)
-#     #assert zip_util.is_tar_file(file) == True
+def test_is_tar_file_true(tmpdir):
+    file = tmpdir.join("test_file.txt")
+    file.write("Hello World!")
+    with tarfile.open("test_file.tar.gz", "w:gz") as tar:
+        tar.add(file)
+    tar.close()
+    tarredfile = tmpdir + "/test_file.tar.gz"
+    # assert zip_util.is_tar_file(tarredfile) == True
